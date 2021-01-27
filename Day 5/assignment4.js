d755810c879546c4fae3368027cbe56d51ffd603
sonal126 <66620112+sonal126@users.noreply.github.com>
2021-01-28 00:14:47 +0530

    Day 5 Assignment

---
+++ Day 5/assignment4.js
@@ -0,0 +1,7 @@
+function add(){
+    var a, b,c;
+    a=Number(document.getElementById("val1").value);
+    b=Number(document.getElementById("val2").value);
+    c=a+b;
+    document.getElementById("answer").value=c;
+}
